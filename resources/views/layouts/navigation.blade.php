89719d55aa9ea750c117a457f487182ea9e5d8a6
Rudisang Morake <rk.morake18@gmail.com>
2021-11-03 21:24:58 +0200

    setup

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="{{ asset('images/gov-logo.png')}}" width=50 alt="">
                     </a>
                 </div>
 
